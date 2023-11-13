c29b63318b47a9e13a04061c16a6d00d7c0d00dd
giwoong <dev.choi0409@gmail.com>
2023-11-13 13:38:04 +0900

    feat:기초 환경 세팅

---
+++ src/main/java/com/example/clouddog/CloudDogApplication.java
@@ -0,0 +1,13 @@
+package com.example.clouddog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clouddog/CloudDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clouddog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
