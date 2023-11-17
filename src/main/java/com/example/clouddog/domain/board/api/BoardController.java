621c0b1241890651b3dbcbbc14e5ce86627619e3
giwoong <dev.choi0409@gmail.com>
2023-11-17 15:29:38 +0900

    refactor: BoardApi 수정

--- src/main/java/com/example/clouddog/domain/board/api/BoardController.java
+++ src/main/java/com/example/clouddog/domain/board/api/BoardController.java
@@ -4,12 +4,18 @@ import com.example.clouddog.domain.board.api.dto.BoardDto;
 import com.example.clouddog.domain.board.api.dto.BoardReqDto;
 import com.example.clouddog.domain.board.api.dto.BoardResDto;
 import com.example.clouddog.domain.board.application.BoardService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -17,46 +23,50 @@ import java.util.List;
 public class BoardController {
 
     private final BoardService boardService;
-    public BoardController(BoardService boardService){
-        this.boardService=boardService;
-    }
 
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
 
 
     //페이징x
     //게시글 목록 불러오기 _ tag가 0이면 전체, 12345면 해당 목록
     @GetMapping("/boards/{bdTag}")
-    public ResponseEntity<List<BoardDto>> board(@PathVariable Integer bdTag){
-        if (bdTag==0)
+    public ResponseEntity<List<BoardDto>> board(@PathVariable Integer bdTag) {
+        if (bdTag == 0) {
             return new ResponseEntity<>(boardService.findAll(), HttpStatus.OK);
-        else
+        } else {
             return new ResponseEntity<>(boardService.findAllByTag(bdTag), HttpStatus.OK);
+        }
     }
 
     //게시글 작성
-    @PostMapping("/board")
-    public ResponseEntity<String> addBoard(@RequestBody BoardReqDto board){
-        boardService.boardSave(board);
+    @PostMapping("/{memberId}/board")
+    public ResponseEntity<String> addBoard(@PathVariable(name = "memberId") Long memberId,
+                                           @RequestBody BoardReqDto board) {
+        boardService.boardSave(memberId, board);
         return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
 
     //게시글 상세 불러오기
-    @GetMapping("/board/{bdId}")
-    public ResponseEntity<BoardResDto> findBoard(@PathVariable Long bdId){
+    @GetMapping("/{memberId}/board/{bdId}")
+    public ResponseEntity<BoardResDto> findBoard(@PathVariable(name = "memberId") Long memberId,
+                                                 @PathVariable Long bdId) {
         BoardResDto boardDto = boardService.findById(bdId);
         return new ResponseEntity<>(boardDto, HttpStatus.OK);
     }
 
     //게시글 수정
-    @PatchMapping("/board/{bdId}")
-    public ResponseEntity<String> updateBoard(@PathVariable Long bdId, @RequestBody BoardReqDto boardDto){
+    @PatchMapping("/{memberId}/board/{bdId}")
+    public ResponseEntity<String> updateBoard(@PathVariable(name = "memberId") Long memberId, @PathVariable Long bdId,
+                                              @RequestBody BoardReqDto boardDto) {
         boardService.boardUpdate(bdId, boardDto);
         return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
 
     //게시글 삭제
-    @DeleteMapping("/board/{bdId}")
-    public ResponseEntity<String> deleteBoard(@PathVariable Long bdId){
+    @DeleteMapping("/{memberId}/board/{bdId}")
+    public ResponseEntity<String> deleteBoard(@PathVariable(name = "memberId") Long memberId, @PathVariable Long bdId) {
         boardService.boardDelete(bdId);
         return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
--- src/main/java/com/example/clouddog/domain/board/api/dto/BoardReqDto.java
+++ src/main/java/com/example/clouddog/domain/board/api/dto/BoardReqDto.java
@@ -4,14 +4,9 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardReqDto {
-
-    private String memberId;
-
     private String bdTitle;
 
     private String bdPlace;
@@ -24,14 +19,13 @@ public class BoardReqDto {
 
     private String bdImageUrl;
 
-    public BoardReqDto(String memberId, String boardTitle, String boardPlace,
-                    int boardTag, String boardContent, String boardTime) {
-        this.memberId=memberId;
-        this.bdTitle=boardTitle;
-        this.bdPlace=boardPlace;
-        this.bdTag=boardTag;
-        this.bdContent=boardContent;
-        this.bdTime=boardTime;
+    public BoardReqDto(String boardTitle, String boardPlace,
+                       int boardTag, String boardContent, String boardTime) {
+        this.bdTitle = boardTitle;
+        this.bdPlace = boardPlace;
+        this.bdTag = boardTag;
+        this.bdContent = boardContent;
+        this.bdTime = boardTime;
         //나중에 이미지 추가
     }
 }
