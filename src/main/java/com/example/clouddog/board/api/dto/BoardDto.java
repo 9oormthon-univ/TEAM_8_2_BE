e403ba68f0614daee5fc3ef74bc663be74b18116
ruth1442 <ruth1442@naver.com>
2023-11-18 18:19:00 +0900

    refactor: 태그 조회 수정

--- src/main/java/com/example/clouddog/board/api/dto/BoardDto.java
+++ src/main/java/com/example/clouddog/board/api/dto/BoardDto.java
@@ -1,5 +1,6 @@
 package com.example.clouddog.board.api.dto;
 
+import com.example.clouddog.image.domain.Image;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,16 +12,19 @@ public class BoardDto {
     private Long bdId;
     private String bdTitle;
     private int bdTag;
-    private String bdImageUrl;
+    private Image bdImage;
 
     public BoardDto(Long bdId,
                     Long memberId,
                     String boardTitle,
-                    int boardTag) {
+                    int boardTag,
+                    Image image
+                    ) {
         this.memberId = memberId;
         this.bdId = bdId;
         this.bdTitle = boardTitle;
         this.bdTag = boardTag;
+        this.bdImage = image;
         //나중에 이미지 추가
     }
 }
